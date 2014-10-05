8b23e3620d0ac03c4fc81df9ab1f44aaa052ad08
cabechler <cathybechler@gmail.com>
2014-10-04 20:29:46 -0400

    making it do stuff
    working on the jQuery so it can add, check off and remove items from the
    list.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+	
 
 
+//add items to list hitting enter.
+	$('#textbox').keydown(function(e){
+		if(e.which == 13){
+			var toAdd = $('input[name=todolistitem]').val();
+			console.log(toAdd)
+			$('.list').append('<li class=\"items\"><div class=\"checkbox\"></div><span class=\"todoitem\">' + toAdd + '</span><img class=\"delete\" src=\"images/delete-button.png\"></li>');
+			$('input#textbox').val('');
+		}
+	});
 
+//delete
+	$('.list').on('click', '.delete', function (e) {
+		e.preventDefault(); 
+		$(this).parent().remove()
+	});
+
+//check off
+	$('.list').on('click', 'li', function (){
+		$(this).toggleClass('completed');
+		$(this).children('.checkbox').toggleClass('checked');
+	});
+	$('#list').sortable({ axis: "y" });
 });
 
